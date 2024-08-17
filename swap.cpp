9b8b38d8ed44bef12d8cbcaf231fe3684517f6ce
Noor185-devi <abbasynoor321@gmail.com>
2024-08-17 21:57:58 +0500

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a=5,b=50;
+ int c;
+ cout<<"Before the Swapping :"<<endl<<"A ="<<a<<endl<<"B ="<<b<<endl<<endl;
+ c=a;
+ a=b;
+ b=c;
+ cout << "After the Swapping :" << endl<<"A ="<<a<<endl<<"B ="<<b;
+
+
+}
