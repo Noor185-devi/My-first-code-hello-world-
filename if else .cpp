d80d2ba9d2f688902ad833edafd177284be1ce38
Noor185-devi <abbasynoor321@gmail.com>
2024-08-17 22:20:50 +0500

    Add files via upload

---
+++ if else .cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int daytime = 20;
+    if (daytime < 18) {
+        cout << "Good day.";
+    } else {
+        cout << "Good evening.";
+    }
+    return 0;
+}
