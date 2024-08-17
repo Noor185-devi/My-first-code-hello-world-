60b26fad9f8cbe94cdbf9372509c5291da5c5658
Noor185-devi <abbasynoor321@gmail.com>
2024-08-17 22:35:18 +0500

    Add files via upload

---
+++ month cleander by switch.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int month;
+  cout<<"Enter month"<<endl;
+  cin>>month;
+  switch (month) {
+  case 1:
+    cout << "January";
+    break;
+  case 2:
+    cout << "February";
+    break;
+  case 3:
+    cout << "March";
+    break;
+  case 4:
+    cout << "April";
+    break;
+  case 5:
+    cout << "May";
+    break;
+  case 6:
+    cout << "June";
+    break;
+  case 7:
+    cout << "july";
+    break; 
+    case 8:
+    cout << "july";
+    break;
+   case 9:
+    cout << "july";
+    break;
+   case 10:
+    cout << "july";
+    break;
+   case 11:
+    cout << "july";
+    break;
+   case 12:
+    cout << "july";
+    break;
+   default:
+    cout << "The end";
+    break;
+    }
+    }
